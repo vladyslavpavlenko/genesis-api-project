562845a1e4f3b495d42c8a658e2f9d9a031f276a
Vladyslav Pavlenko <vladyslavpavlen@gmail.com>
2024-05-16 11:22:56 +0300

    initial commit

---
+++ cmd/api/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+	"github.com/vladyslavpavlenko/genesis-api-project/internal/config"
+	"github.com/vladyslavpavlenko/genesis-api-project/internal/handlers"
+	"net/http"
+)
+
+func routes(app *config.AppConfig) http.Handler {
+	mux := chi.NewRouter()
+	mux.Use(middleware.Logger)
+
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://*", "http://*"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+	}))
+
+	mux.Get("/rate", handlers.Repo.GetRate)
+	mux.Post("/subscribe", handlers.Repo.Subscribe)
+	mux.Post("/sendEmails", handlers.Repo.SendEmails)
+
+	return mux
+}
---
+++ cmd/api/setup.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"github.com/joho/godotenv"
+	"github.com/vladyslavpavlenko/genesis-api-project/internal/config"
+	"github.com/vladyslavpavlenko/genesis-api-project/internal/handlers"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"log"
+	"os"
+)
+
+func setup(app *config.AppConfig) error {
+	// Get environment variables
+	env, err := loadEvnVariables()
+	if err != nil {
+		return err
+	}
+
+	app.Env = env
+
+	// Connect to the database and run migrations
+	db, err := connectToPostgresAndMigrate(env)
+	if err != nil {
+		return err
+	}
+
+	app.DB = db
+
+	repo := handlers.NewRepo(app)
+	handlers.NewHandlers(repo)
+	render.NewRenderer(app)
+
+	return nil
+}
+
+// loadEvnVariables loads variables from the .env file.
+func loadEvnVariables() (*config.EnvVariables, error) {
+	err := godotenv.Load()
+	if err != nil {
+		return nil, fmt.Errorf("error getting environment variables: %v", err)
+	}
+
+	postgresHost := os.Getenv("POSTGRES_HOST")
+	postgresUser := os.Getenv("POSTGRES_USER")
+	postgresPass := os.Getenv("POSTGRES_PASS")
+	postgresDBName := os.Getenv("POSTGRES_DBNAME")
+	jwtSecret := os.Getenv("JWT_SECRET")
+
+	return &config.EnvVariables{
+		PostgresHost:   postgresHost,
+		PostgresUser:   postgresUser,
+		PostgresPass:   postgresPass,
+		PostgresDBName: postgresDBName,
+	}, nil
+}
+
+// connectToPostgresAndMigrate initializes a PostgreSQL db session and runs GORM migrations.
+func connectToPostgresAndMigrate(env *config.EnvVariables) (*gorm.DB, error) {
+	dsn := fmt.Sprintf("host=%s user=%s dbname=%s password=%s sslmode=disable",
+		env.PostgresHost, env.PostgresUser, env.PostgresDBName, env.PostgresPass)
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("could not connect: ", err)
+	}
+
+	return db, nil
+}
